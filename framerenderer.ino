e116bed01313c80737f3097936846f2f97ff89c0
E. Almqvist <h1g.server@gmail.com>
2020-04-19 19:21:54 +0200

    Added random color beam

--- framerenderer.ino
+++ framerenderer.ino
@@ -1,5 +1,4 @@
 #include <FastLED.h>
-//#include <Math>
 
 // Hardware vars
 #define CHIPSET WS2812
@@ -8,39 +7,51 @@
 #define DATA_PIN 2
 
 // Vars
-CRGB leds[NUM_LEDS];
+CRGB leds[NUM_LEDS] = { CRGB( 0, 0, 0 ) };
 
 	
 // Config
-#define BRIGHTNESS 20
+#define BRIGHTNESS 128
+#define updateLEDS 6 // update speed
+#define beamLength 8
+#define DELAY 1500 // max delay
 
+#define clrRandom random( 0, 255 )
+CRGB genRandomClr() {
+	return CRGB( clrRandom, clrRandom, clrRandom );
+}
+
+void addBeam( CRGB clr = CRGB( 0, 0, 255 ), int pos = 0 ) {
+	for( int i = pos; i < beamLength; i++ ) {
+		leds[i] = clr;
+	}
+}
+
+unsigned long lastTime;
 void setup() {
 	FastLED.addLeds<CHIPSET, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
 	FastLED.setBrightness(BRIGHTNESS);
+	
+	CRGB clr = genRandomClr();
+	addBeam( clr );
 
-//	for( int i = 0; i < NUM_LEDS; i++ ) {
-//		leds[i] = CRGB( 0, 0, 0 );
-//	}
-	leds[0] = CRGB( 0, 0, 255 );
-	leds[1] = CRGB( 0, 0, 255 );
+	lastTime = millis();
 
-	leds[15] = CRGB( 0, 255, 0 );
-	leds[16] = CRGB( 0, 255, 0 );
-//	FastLED.show();
+	FastLED.show();
 }
 
-
-// Render frame
-
 void loop() {
-	
-	for( int i = 0; i < NUM_LEDS; i++ ) {
-		leds[i+1] = leds[i - 1];
-		FastLED.show();
-		delay(1);
-		leds[i-1] = CRGB( 0, 0, 0 );
-		FastLED.show();
-		delay(1);
+	FastLED.setBrightness(random(0, BRIGHTNESS));
+	for( int i = NUM_LEDS - 1; i >= updateLEDS; i-- ) {
+		leds[i] = leds[i - updateLEDS];
+		leds[i - updateLEDS] = CRGB( 0, 0, 0 ); // remove the old led
 	}
-	//FastLED.show(); // show it	
+	
+	if( millis() - lastTime >= random( 40, DELAY ) ) {
+		addBeam(genRandomClr());
+		lastTime = millis();
+	}
+
+	FastLED.show(); // show it	
+	//delay(10);
 }
